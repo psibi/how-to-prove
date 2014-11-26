29bd081533af9ee689cd1fbb4569cff321c4db80
Sibi <sibi@psibi.in>
2014-11-26 23:15:58 +0530

    Factor code.

--- chapter 3/factor.hs
+++ chapter 3/factor.hs
@@ -1,8 +1,16 @@
 findFactors :: Integral a =>  a -> [a]
-findFactors num = take 1 $ filter (\x -> num `mod` x == 0) [2..num]
+findFactors num = take 1 $ filter (\x -> num `mod` x == 0 && x /= num) [2..num]
 
 hasFactor :: Integral a => a -> Bool                  
 hasFactor x = if (null (findFactors x))
               then False
               else True
 
+isPrime :: Integral a => a -> Bool
+isPrime x = not $ hasFactor x
+
+problem3 :: Num a => a -> a
+problem3 n = 2*n + 13
+
+primeProblem3 :: [Integer]
+primeProblem3 = take 1 $ filter isPrime $ map problem3 [3..]
